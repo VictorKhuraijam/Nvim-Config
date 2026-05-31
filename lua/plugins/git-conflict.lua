c0d0382fb242aebca623ba6f4c5ce8b36a805c14
VictorKhuraijam <khuraijamvictor@gmail.com>
2026-05-31 23:19:48 +0530

    added git-conflict.lua file for git merge/conflict

---
+++ lua/plugins/git-conflict.lua
@@ -0,0 +1,15 @@
+return {
+	"akinsho/git-conflict.nvim",
+	version = "*", -- Uses the latest stable release
+	config = function()
+		require("git-conflict").setup({
+			default_mappings = true, -- Automatically sets up the standard TUI keymaps
+			disable_diagnostics = true, -- Turns off LSP syntax errors inside conflict zones
+			list_opener = "copen", -- Automatically opens the quickfix list for multi-file conflicts
+			highlights = { -- Optional: Customize your TUI look (uses terminal background colors)
+				incoming = "DiffText",
+				current = "DiffAdd",
+			},
+		})
+	end,
+}
