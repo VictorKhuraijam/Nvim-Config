5d5c5fbd1d88ad317536da78eb572c3f73ec53cb
VictorKhuraijam <khuraijamvictor@gmail.com>
2026-06-08 23:08:14 +0530

    changes in the nvim-tree.lua file

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -11,62 +11,63 @@ return {
 	config = function()
 		-- Remove background color from the NvimTree window (ui fix)
 
-    vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=NONE]])
+		vim.cmd([[hi NvimTreeNormal guibg=NONE ctermbg=NONE]])
 
-    require("nvim-web-devicons").setup({
-      override = {
-        yaml = {
-          icon = "",
-          color = "#6d8086",
-          name = "Yaml",
-        },
-        yml = {
-          icon = "",
-          color = "#6d8086",
-          name = "Yml",
-        },
-      },
-    })
+		require("nvim-web-devicons").setup({
+			override = {
+				yaml = {
+					icon = "",
+					color = "#6d8086",
+					name = "Yaml",
+				},
+				yml = {
+					icon = "",
+					color = "#6d8086",
+					name = "Yml",
+				},
+			},
+		})
 
 		require("nvim-tree").setup({
-     renderer = {
-        icons = {
-          show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = true,
-          },
-        },
-      },
-  		git = {
-        ignore = false,
-      },
-      filters = {
+			renderer = {
+				icons = {
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+				},
+			},
+			git = {
+				ignore = false,
+			},
+			filters = {
 				dotfiles = false, -- Show hidden files (dotfiles)
 			},
 			view = {
 				adaptive_size = true,
 			},
-      ui = {
-          confirm = {
-            remove = true,
-            trash = true,
-          },
-        },
-        -- Use floating input window instead of command line
-        on_attach = function(bufnr)
-          local api = require("nvim-tree.api")
-          local function opts(desc)
-            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-          end
+			ui = {
+				confirm = {
+					remove = true,
+					trash = true,
+				},
+			},
+			-- Use floating input window instead of command line
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
 
-          -- Default mappings
-          api.config.mappings.default_on_attach(bufnr)
+				-- Default mappings
+				api.config.mappings.default_on_attach(bufnr)
 
-          -- Override 'a' to use floating input
-          vim.keymap.set("n", "a", api.fs.create, opts("Create"))
-        end,
-      })
-    end,
-  }
+				-- Override 'a' to use floating input
+				vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+			end,
+		})
+	end,
+}
+
